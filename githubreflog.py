cb6dd06ff6ee7a9b14331a50f198638a519cdc08
Jim Zucker <jim@stratengllc.com>
2016-03-14 11:11:25 -0700

    Cleaned up exception from pipe to less

--- githubreflog.py
+++ githubreflog.py
@@ -8,42 +8,52 @@ BRANCHCOLW=20
 MSGCOLW=60
 DFLTCOLW=10
 
-SKIPEVENTS=["ForkEvent","IssuesEvent","WatchEvent","IssueCommentEvent","PullRequestEvent","PullRequestReviewCommentEvent"]
+HANDLEDEVENTS=["CreateEvent","DeleteEvent","PushEvent","CommitCommentEvent"]
+#SKIPEVENTS=["ForkEvent","IssuesEvent","WatchEvent","IssueCommentEvent","PullRequestEvent","PullRequestReviewCommentEvent"]
 
+#Function to print the output
 def printIt( sha, type, repo, branch, login, message, filler):
+
+	#clean up comments that have \n in them
 	message = message.replace("\n"," ")
+
 	print Fore.RED+sha[:SHACOLW].ljust(SHACOLW," ")+Fore.RESET,type[:EVENTCOLW].ljust(EVENTCOLW,filler),repo[:DFLTCOLW].ljust(DFLTCOLW,filler),branch[:BRANCHCOLW].ljust(BRANCHCOLW,filler),Fore.BLUE+login.ljust(DFLTCOLW," ")+Fore.RESET,message.ljust(MSGCOLW," ")
 	return
 
-rows=json.load(sys.stdin)
 printIt("sha","type","repo","branch","login","message"," ")
+
+rows=json.load(sys.stdin)
 for r in rows: 
 	type = r.get("type","")
-	if type in SKIPEVENTS: 
+	if type not in HANDLEDEVENTS: 
 		continue
+
 	repo = r.get("repo","").get("name","").split("/")[1][:DFLTCOLW]
 	login = r["actor"]["login"][:DFLTCOLW]
+
+	#set message to Date/Time for event lines
 	message = r.get("created_at")
+
 	payload = r.get("payload")
-	if type == "CommitCommentEvent":
-		comment = payload.get("comment")
-		sha = comment.get("commit_id")
-		branch = ""
-		printIt(sha, type, repo, branch, login, message, " ")
-	else:
-		if payload :
+	if payload :
+		#CommitCommentEvent has a different payload
+		if type == "CommitCommentEvent":
+			comment = payload.get("comment")
+			sha = comment.get("commit_id")
+			branch = ""
+			printIt(sha, type, repo, branch, login, message, " ")
+		else:
+			#Enhance message for a Create Event to show the source branch
+			if type == "CreateEvent":
+				message = message + " " + payload.get("ref_type","") + ":"  + payload.get("master_branch","")
 			sha = payload.get("head","")[:SHACOLW]
 			branch = payload.get("ref")
 			if branch:
 				refsplit = branch.split("/")
 				if len(refsplit) == 3:
 					branch = refsplit[2]
-		else:
-			sha = ""
-			branch = ""
-		printIt(sha, type, repo, branch, login, message," ")
+			printIt(sha, type, repo, branch, login, message," ")
 
-		if payload:
 			commits = payload.get("commits")
 			if commits:
 				for c in commits:
@@ -51,3 +61,10 @@ for r in rows:
 						shaCommit = c["sha"][:SHACOLW]
 						message = c["message"][:MSGCOLW]
 						printIt(shaCommit,"", "", "", login,message,".")
+	else:
+		sha = ""
+		branch = ""
+		printIt(sha, type, repo, branch, login, message," ")
+
+exit(0)
+
