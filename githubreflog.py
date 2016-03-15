c923dafdb92c0a84e434292ae6fe3d41bdea6f96
Jim Zucker <jim@stratengllc.com>
2016-03-14 19:39:40 -0700

    Added paging via less

--- githubreflog.py
+++ githubreflog.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python
 import sys, json, requests
 from colorama import Fore, Back, Style
+from subprocess import Popen, PIPE
+import errno
 
 #----------------------------------------------------------------------------------------------------------------------
 SHACOLW=7
@@ -13,13 +15,29 @@ HANDLEDEVENTS=["CreateEvent","DeleteEvent","PushEvent","CommitCommentEvent"]
 #SKIPEVENTS=["ForkEvent","IssuesEvent","WatchEvent","IssueCommentEvent","PullRequestEvent","PullRequestReviewCommentEvent"]
 
 #Function to print the output
-def printIt( sha, type, repo, branch, login, message, filler):
+def printIt( p, sha, type, repo, branch, login, message, filler):
 
 	#clean up comments that have \n in them
 	message = message.replace("\n"," ")
 
-	print Fore.RED+sha[:SHACOLW].ljust(SHACOLW," ")+Fore.RESET,type[:EVENTCOLW].ljust(EVENTCOLW,filler),repo[:DFLTCOLW].ljust(DFLTCOLW,filler),branch[:BRANCHCOLW].ljust(BRANCHCOLW,filler),Fore.BLUE+login.ljust(DFLTCOLW," ")+Fore.RESET,message.ljust(MSGCOLW," ")
+	#print Fore.RED+sha[:SHACOLW].ljust(SHACOLW," ")+Fore.RESET,type[:EVENTCOLW].ljust(EVENTCOLW,filler),repo[:DFLTCOLW].ljust(DFLTCOLW,filler),branch[:BRANCHCOLW].ljust(BRANCHCOLW,filler),Fore.BLUE+login.ljust(DFLTCOLW," ")+Fore.RESET,message.ljust(MSGCOLW," ")
+	line = Fore.RED+sha[:SHACOLW].ljust(SHACOLW," ")+Fore.RESET
+	line += " " + type[:EVENTCOLW].ljust(EVENTCOLW,filler)
+	line += " " + repo[:DFLTCOLW].ljust(DFLTCOLW,filler)
+	line += " " + branch[:BRANCHCOLW].ljust(BRANCHCOLW,filler)
+	line += " " + Fore.BLUE+login.ljust(DFLTCOLW," ")+Fore.RESET
+	line += " " + message.ljust(MSGCOLW," ")
+	line += "\n"
+
+	try:
+		p.stdin.write(line)
+	except:
+		# Stop loop on "Invalid pipe" or "Invalid argument".
+		# No sense in continuing with broken pipe.
+		exit(1)
+
 	return
+
 
 #----------------------------------------------------------------------------------------------------------------------
 
@@ -34,10 +52,13 @@ if len(sys.argv) == 3:
 url="https://api.github.com/repos/" + gitUser + "/" + gitRepo + "/events?per_page=100"
 response = requests.get(url, stream=True)
 
+#open a pipe
+p = Popen('less', stdin=PIPE)
+
 #get the total # of pages from the header
 numPages = int( response.headers["link"].split(",")[1].split("?")[1].split("&")[1].split("=")[1].split(">")[0])
 for i in range(1,numPages):
-	printIt("sha","type","repo","branch","login","message"," ")
+	printIt(p, "sha", "type", "repo", "branch", "login", "message", " ")
 
 	if i > 1: 
 		response = requests.get(url+'&page='+str(i), stream=True) 
@@ -61,7 +82,7 @@ for i in range(1,numPages):
 				comment = payload.get("comment")
 				sha = comment.get("commit_id")
 				branch = ""
-				printIt(sha, type, repo, branch, login, message, " ")
+				printIt(p, sha, type, repo, branch, login, message, " ")
 			else:
 				#Enhance message for a Create Event to show the source branch
 				if type == "CreateEvent":
@@ -72,7 +93,7 @@ for i in range(1,numPages):
 					refsplit = branch.split("/")
 					if len(refsplit) == 3:
 						branch = refsplit[2]
-				printIt(sha, type, repo, branch, login, message," ")
+				printIt(p, sha, type, repo, branch, login, message," ")
 
 				commits = payload.get("commits")
 				if commits:
@@ -80,11 +101,15 @@ for i in range(1,numPages):
 						if c:
 							shaCommit = c["sha"][:SHACOLW]
 							message = c["message"][:MSGCOLW]
-							printIt(shaCommit,"", "", "", login,message,".")
+							printIt(p, shaCommit,"", "", "", login,message,".")
 		else:
 			sha = ""
 			branch = ""
-			printIt(sha, type, repo, branch, login, message," ")
+			printIt(p, sha, type, repo, branch, login, message," ")
+
+#clean up the pipe
+p.stdin.close()
+p.wait()
 
 exit(0)
 
